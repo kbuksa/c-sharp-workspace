15d263559b9e753c5fd9ebad531075bbf07e38c9
kbuksa <kacperbuksa@outlook.com>
2023-10-04 19:24:12 +0100

    added course exercise files I will be following to learn C#

---
+++ Course Exercise Files/Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// Namespace containing fundamental types & 
+// classes of .NET Framework libraries
+using System;
+
+// New class called Program
+class Program {
+
+    // Main method which outputs Hello World
+    static void Main(string[] args){
+        Console.WriteLine("Hello New World!");
+        Console.WriteLine("What's your name?");
+        #nullable disable
+        string str = Console.ReadLine();
+        #nullable restore
+        Console.WriteLine("Hello there " + str + "!");
+    }
+}
